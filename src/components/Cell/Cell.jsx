c8ea2db9024c9daebb8c3738facd979424b56cab
Slava Biryukov <networmx128bit@gmail.com>
2020-07-20 19:58:46 +0300

    Validation tryout #1

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -10,6 +10,7 @@ const StyledCell = styled.div`
   white-space: nowrap;
   box-sizing: border-box;
   outline: 1px solid #000;
+  background-color: ${({ isValid }) => isValid ? 'green' : 'transparent'};
   
   &:after {
     display: block;
@@ -41,29 +42,40 @@ const StyledInput = styled.input`
   }
 `
 
-export function Cell ({row, column, onChange}) {
+export function Cell ({row, column, onChange, validityInstances = []}) {
   const id = `cell-r${row}-c${column}`
+  const isValid = !!validityInstances.find(matchLine => matchLine.isValid)
 
   if (!row) {
-    return <StyledColumnHeading id={id}>
+    return <StyledColumnHeading
+      id={id}
+      isValid={isValid}
+    >
       {config.columnRegularExpressions[column - 1]}
     </StyledColumnHeading>
   }
 
   if (row && !column) {
-    return <StyledRowHeading id={id}>
+    return <StyledRowHeading
+      id={id}
+      isValid={isValid}
+    >
       {config.rowRegularExpressions[row - 1]}
     </StyledRowHeading>
   }
 
   if (row && column) {
-    return <StyledCell id={id}>
+    return <StyledCell
+      id={id}
+      isValid={isValid}
+    >
       <StyledInput
         type="text"
         pattern="(1|0)"
         maxLength="1"
-        onChange={event => onChange(event.target.value)}
+        onChange={event => onChange(event.target.value) || console.log(133, validityInstances)}
       />
+      <div style={{position: 'absolute', pointerEvents: 'none'}}>{validityInstances.map(i => i.isValid.toString()).join(', ')}</div>
     </StyledCell>
   }
 }
--- src/components/Crossword/Crossword.jsx
+++ src/components/Crossword/Crossword.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { config } from '../../config'
 import { Cell } from '../Cell'
+import { MatchLine } from '../../common/MatchLine'
 
 const StyledCrosswordContainer = styled.main`
   flex: 1 100%;
@@ -20,17 +21,48 @@ const StyledCrosswordField = styled.figure`
 
 const cells = []
 
+const rows = (new Array(config.rowsCount))
+  .fill()
+  .map((item, index) => {
+    const line = new MatchLine(config.rowRegularExpressions[index])
+    line.items = (new Array(config.columnsCount)).fill(null)
+    return line
+  })
+
+const columns = (new Array(config.columnsCount))
+  .fill()
+  .map((item, index) => {
+    const line = new MatchLine(config.columnRegularExpressions[index])
+    line.items = (new Array(config.rowsCount)).fill(null)
+    return line
+  })
+
+
 const onCellChanged = (row, column, value) => {
+  rows[row - 1].setItem(column - 1, value)
+  columns[column - 1].setItem(row - 1, value)
+
   console.log(`value changed at row ${row}, column ${column}:`, value)
+  console.log({
+    rows,
+    columns,
+    isValid: (row && rows[row - 1].isValid) || (column && columns[column - 1].isValid)
+  })
 }
 
 for (let row = 0; row <= config.rowsCount; row++) {
   for (let column = 0; column <= config.columnsCount; column++) {
+    const validityInstances = []
+
+    if (row) { validityInstances.push(rows[row - 1]) }
+    if (column) { validityInstances.push(columns[column - 1]) }
+
     cells.push(
       <Cell
         key={`cell-${row}:${column}`}
         row={row}
         column={column}
+        validityInstances={validityInstances}
         onChange={onCellChanged.bind(null, row, column)}
       />
     )
