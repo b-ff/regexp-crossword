40e02306cf0ce54a736dba1e6c504f80b82b43b7
Slava Biryukov <networmx128bit@gmail.com>
2020-07-21 16:40:27 +0300

    Updated dependencies

---
+++ docs/precache-manifest.11063959b6c4cf5e84a9e7500f0c8104.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a635df2eef6d1ad30da7621e2f2ae673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbf836526ba9e8499d87",
+    "url": "/static/js/2.08cb3526.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08cb3526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb344bb512f68a6ebc8",
+    "url": "/static/js/main.5219de7d.chunk.js"
+  },
+  {
+    "revision": "a789cf34234c68c0a5c9",
+    "url": "/static/js/runtime-main.39785961.js"
+  }
+]);
